90b3067bf7696c5d611938ce31e262074f8c1a96
EduHC <eduardo.cic.horn@gmail.com>
2021-01-17 00:36:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department object = new Department("Books", 1);
-		System.out.println(object);
+		Seller seller = new Seller(10, "Eduardo", "eduardo@mail.com", new Date(), 3000.00, object);
+		
+		System.out.println(seller);
 	}
 }
