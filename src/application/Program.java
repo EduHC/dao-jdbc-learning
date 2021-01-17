842ff7c44c421ca77911dc9eee5167c8f23668d0
EduHC <eduardo.cic.horn@gmail.com>
2021-01-17 02:14:49 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,9 @@ public class Program {
 	public static void main(String[] args) {
 		Department object = new Department("Books", 1);
 		Seller seller = new Seller(10, "Eduardo", "eduardo@mail.com", new Date(), 3000.00, object);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao(); 
+	// utilizando o Factory faz-se uma dependency injection e o programa não conhece a implementação, mas apenas a interface
 		
 		System.out.println(seller);
 	}
